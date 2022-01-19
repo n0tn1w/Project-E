898f22fc001c27f411d343b373d478241ad11bc9
n0tn1w <n0tn1wofficial@gmail.com>
2022-01-19 15:35:17 +0200

    Login, Quit and hole Home Screen is done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-string splitBy(string input, char symbol){
+string getNameWithLineInput(string input, char symbol){
     int i = 0;
     for (auto &ch : input) {
         if(ch == symbol){
@@ -22,13 +22,40 @@ bool checkIfTheNameAlreadyExist(string name){
     dataBase.open("user.txt", fstream::in | fstream::app);
     string line;
     while(getline(dataBase, line)){
-        if(name == splitBy(line, ':')){
+        if(name == getNameWithLineInput(line, ':')){
             return false;
-
         }
     }
-
     return true;
+}
+
+string getHashedPassword(string name){
+    fstream dataBase;
+    dataBase.open("user.txt", fstream::in | fstream::app);
+    string line;
+
+    string correctLine;
+    while(getline(dataBase, line)){
+        if(name == getNameWithLineInput(line, ':')){
+            correctLine = line;
+        }
+    }
+    int i = 0;
+    for (auto &ch : correctLine) {
+        if(ch == ':'){
+            break;
+        }
+        i++;
+    }
+    return correctLine.substr(i + 1, correctLine.length());
+}
+bool checkIfPasswordIsCorrect(string name ,string passwordGuess){
+    hash <string> hash;
+    long long passwordGuessHashNumber = hash(passwordGuess);
+    string passwordGuessHashString = to_string(passwordGuessHashNumber);
+
+    string passwordRealHash = getHashedPassword(name);
+    return passwordRealHash == passwordGuessHashString;
 }
 
 bool registeer(){
@@ -55,23 +82,49 @@ bool registeer(){
         string fileName = name + ".txt";
         dataBase.open(fileName, fstream::app);
         dataBase.close();
+        cout << "Register successfully!" << endl;
         return true;
     }else if(!checkIfTheNameAlreadyExist(name)){
-        cout << "User already exist!"<< endl;
+        cout << "User already exist!" << endl;
         return false;
     }else if(!regex_match(name, nameRegex)){
-        cout << "Invalid name!"<< endl;
+        cout << "Invalid name!" << endl;
         return false;
     }else if(!regex_match(password, passwordRegex)){
-        cout << "Invalid password!"<< endl;
+        cout << "Invalid password!" << endl;
         return false;
     }else{
-        cout << "Incorrect username or password!!!"<< endl;
+        cout << "Incorrect username or password!!!" << endl;
         return false;
     }
+}
 
 
+bool login(){
+    fstream dataBase;
+    string name, password;
 
+    cout << "Enter username: ";
+    getline(cin, name);
+    cout << "Enter password: ";
+    getline(cin, password);
+
+    dataBase.open("user.txt", fstream::in | fstream::app);
+    if(!checkIfTheNameAlreadyExist(name)
+        && checkIfPasswordIsCorrect(name ,password)){
+        cout << "Login successfully!" << endl;
+        return true;
+
+    }else if(checkIfTheNameAlreadyExist(name)){
+        cout << "This user doesnt exist!" << endl;
+        return false;
+    }else if(!checkIfPasswordIsCorrect(name ,password)){
+        cout << "Wrong password!" << endl;
+        return false;
+    }else{
+        cout << "Incorrect username or password!" << endl;
+        return false;
+    }
 }
 
 int main()
@@ -80,14 +133,27 @@ int main()
     dataBase.open("user.txt", fstream::in | fstream::app);
     dataBase.close();
 
-    cout << "Enter a command: ";
-    string command;
-    getline(cin, command);
+    bool areUOnTheHomePage = false;
+    //!areUOnTheHomePage
+    cout << "-------------------------" << endl;
+    cout << "Welcome to Home Scree :)" << endl;
+    while(!false){
+        cout << "Enter a command: ";
+        string command;
+        getline(cin, command);
 
-    while(true){
-        cout << "Time to register" << endl;
-        registeer();
+        if(command == "r" || command == "R"){
+            areUOnTheHomePage = registeer();
+        }else if(command == "l" || command == "L"){
+            areUOnTheHomePage = login();
+        }else if(command == "q" || command == "Q"){
+            return 0;
+        }else{
+            cout << "Do u have hands?";
+
+        }
     }
+
 
     return 0;
 }
