f090f434cd39ab76a47415c1c0703938d6cb8f6b
n0tn1w <n0tn1wofficial@gmail.com>
2022-01-19 18:09:20 +0200

    Close account function is done

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include <regex>
 #include <stdio.h>
 #include <string.h>
+#include <cstdio>
 
 using namespace std;
+
+string longgedUser;
 
 string getNameWithLineInput(string input, char symbol){
     int i = 0;
@@ -58,6 +61,31 @@ bool checkIfPasswordIsCorrect(string name ,string passwordGuess){
     return passwordRealHash == passwordGuessHashString;
 }
 
+bool removeAUserFromTheDB(){
+    fstream dataBase;
+    dataBase.open("user.txt", fstream::in | fstream::app);
+    vector<string> dbSave;
+    string line;
+
+    while(getline(dataBase, line)){
+            cout << "Name: " <<getNameWithLineInput(line, ':') << " ";
+        if(longgedUser == getNameWithLineInput(line, ':')){
+            cout << "removed" << line << endl;
+            continue;
+        }else{
+            dbSave.push_back(line);
+            cout << line << endl;
+        }
+    }
+    dataBase.close();
+    dataBase.open("user.txt", fstream::out | fstream::trunc);
+    for(int i = 0;i < dbSave.size(); i++){
+        dataBase << dbSave[i] << "\n";
+    }
+    dataBase.close();
+    return true;
+}
+
 bool registeer(){
     hash <string> hash;
     fstream dataBase;
@@ -70,6 +98,7 @@ bool registeer(){
     getline(cin, name);
     cout << "Enter password: ";
     getline(cin, password);
+    longgedUser = name;
 
     if(regex_match(name, nameRegex)
        && regex_match(password, passwordRegex)
@@ -94,7 +123,7 @@ bool registeer(){
         cout << "Invalid password!" << endl;
         return false;
     }else{
-        cout << "Incorrect username or password!!!" << endl;
+        cout << "Incorrect action!" << endl;
         return false;
     }
 }
@@ -108,6 +137,7 @@ bool login(){
     getline(cin, name);
     cout << "Enter password: ";
     getline(cin, password);
+    longgedUser = name;
 
     dataBase.open("user.txt", fstream::in | fstream::app);
     if(!checkIfTheNameAlreadyExist(name)
@@ -122,7 +152,33 @@ bool login(){
         cout << "Wrong password!" << endl;
         return false;
     }else{
-        cout << "Incorrect username or password!" << endl;
+        cout << "Incorrect action!" << endl;
+        return false;
+    }
+}
+
+bool closeAccount(){
+    fstream dataBase;
+    string password;
+    cout << "Enter password to confirm: ";
+    getline(cin, password);
+
+    if(checkIfPasswordIsCorrect(longgedUser ,password)){
+        if(removeAUserFromTheDB()){
+            string fileName = longgedUser + ".txt";
+            std::remove(fileName.c_str());
+            cout << "Account deleted successfully!" << endl;
+            return true;
+        }else{
+            cout << "Error in the DataBase!" << endl;
+            return false;
+        }
+
+    }else if(!checkIfPasswordIsCorrect(longgedUser ,password)){
+        cout << "Wrong password!" << endl;
+        return false;
+    }else{
+        cout << "Incorrect action!" << endl;
         return false;
     }
 }
@@ -133,25 +189,53 @@ int main()
     dataBase.open("user.txt", fstream::in | fstream::app);
     dataBase.close();
 
-    bool areUOnTheHomePage = false;
+    bool areUOnTheHomePage = true;
+    bool stay = true;
     //!areUOnTheHomePage
-    cout << "-------------------------" << endl;
-    cout << "Welcome to Home Scree :)" << endl;
-    while(!false){
-        cout << "Enter a command: ";
-        string command;
-        getline(cin, command);
+    while(true){
+        if(areUOnTheHomePage){
+            if(stay){
+                cout << "-------------------------" << endl;
+                cout << "Welcome to Home Screen :)" << endl;
+            }
+            cout << "Enter a command: ";
+            string command;
+            getline(cin, command);
 
-        if(command == "r" || command == "R"){
-            areUOnTheHomePage = registeer();
-        }else if(command == "l" || command == "L"){
-            areUOnTheHomePage = login();
-        }else if(command == "q" || command == "Q"){
-            return 0;
+            if(command == "r" || command == "R"){
+                areUOnTheHomePage = !registeer();
+            }else if(command == "l" || command == "L"){
+                areUOnTheHomePage = !login();
+            }else if(command == "q" || command == "Q"){
+                return 0;
+            }else{
+                //cout << "Do u have hands?";
+                cout << "Invalid command!" << endl;
+            }
+            stay = false;
         }else{
-            cout << "Do u have hands?";
+            if(!stay){
+                cout << "-------------------------" << endl;
+                cout << "Welcome to Work Screen 8]" << endl;
+                cout << "Hello " << longgedUser << endl;
+                cout << "You have X mails. Choose one of the following options:" << endl;
+                cout << "C - close account" << endl;
+                cout << "I - inbox" << endl;
+                cout << "L - logout" << endl;
+                cout << "O - open" << endl;
+                cout << "S - send" << endl;
+            }
+            cout << "Enter a command: ";
+            string command;
+            getline(cin, command);
 
+            if(command == "c" || command == "C"){
+                areUOnTheHomePage = closeAccount();
+            }
+
+            stay = true;
         }
+
     }
 
 
